2d28286dca99189c10d9da012348599f6b06df17
bandarimanojkumar <bandarimanojkumar25@gmail.com>
2015-09-16 18:31:19 +0530

    Update input4.js

--- lab02/input4.js
+++ lab02/input4.js
@@ -2,19 +2,25 @@ var b = require('bonescript');
 b.pinMode('P8_19', b.INPUT);
 b.pinMode('P8_13', b.OUTPUT);
 b.pinMode('P9_16', b.OUTPUT);
+var status = 0;
+
 setInterval(check,100);
 
 function check(){
 b.digitalRead('P8_19', checkButton);
+status = b.digitalRead('P8_16');
 }
 
 function checkButton(x) {
   if(x.value == 1){
-    b.digitalWrite('P8_13', b.HIGH);
-    b.digitalWrite('P9_16', b.HIGH);
-  }
-  else{
-    b.digitalWrite('P8_13', b.LOW);
-     b.digitalWrite('P9_16', b.LOW);
+   blinkLed();
   }
 }
+
+function blinkLed(){
+  while(status) {
+   b.digitalWrite('P8_13', b.HIGH);
+   delay(10);
+    b.digitalWrite('P8_13', b.Low);
+    delay(10);
+  }
